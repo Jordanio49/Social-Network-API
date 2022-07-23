7d7f26cc6d869db418c8b5667f7e3547325964d8
Jordan Thomas <jthomas135@hotmail.com>
2022-07-23 14:41:46 -0700

    small adjustments

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/Social-Network-API', {
-    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
