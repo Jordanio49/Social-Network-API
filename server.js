5703f1b465870b90dac147073176ca55b19a0251
Jordan Thomas <jthomas135@hotmail.com>
2022-07-23 19:52:52 -0700

    trying to get server started

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const routes = require('./routes')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(routes);
 
-app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/Social-Network-API', {
     // useFindAndModify: false,
